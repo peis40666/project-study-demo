75af1ffdc5b53c39e37529c64c1cd3c2a67bc74c
wangpei@citms.cn <wangpei@citms.cn>
2020-04-27 22:18:29 +0800

    cglib实现动态代理

--- proxy/src/main/java/com/proxy/dynamic/game/CglibClient.java
+++ proxy/src/main/java/com/proxy/dynamic/game/CglibClient.java
@@ -2,9 +2,13 @@ package com.proxy.dynamic.game;
 
 import com.proxy.statics.game.GamePlayer;
 import com.proxy.statics.game.IGamePlayer;
+import org.springframework.cglib.core.DebuggingClassWriter;
 
 public class CglibClient {
     public static void main(String[] args) {
+        //将内存中的class写入磁盘
+        System.setProperty(DebuggingClassWriter.DEBUG_LOCATION_PROPERTY,"/Users/wangpei/Downloads/cglib");
+
         IGamePlayer proxy = (IGamePlayer) new GamePlayerCglibDynProxy().getProxyInstance(new GamePlayer("小A"));
         System.out.println(proxy.getClass().getName());  //这里我打印了代理类的类名
         proxy.login("我是小A呀","123");  //登陆账号
